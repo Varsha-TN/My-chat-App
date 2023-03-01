6c100feb5bcb71df390d92e59b4f71505dc9f94e
Varsha <varsha.inno@aesl.in>
2023-03-01 15:24:11 +0530

    added file upload

--- src/App.js
+++ src/App.js
@@ -1,32 +1,78 @@
 import React, { useState } from 'react';
 import './App.css';
-import Sidebar from './components/Sidebar';
+import { SidebarData } from "./components/SidebarData";
+
 
 
 function App() {
-
-  const [text, setText] = useState("");
-  const [sendMsg, setSendMsg] = useState([]);
-
+  const [message, setMessage] = useState("");
+  const [activeUser, setActiveUser] = useState("");
+  const [messageArr, setMessageArr] = useState([]);
+  const [file, setFile] = useState("");
 
   const handleInputChange = (e) => {
-    setText(e.target.value);
+    setMessage(e.target.value);
+  };
+
+  function handleUpload(event) {
+    setFile(event.target.files[0]);
   }
-  
-const handleSendMsg =() => {
-  setSendMsg([...sendMsg, text]);
-}
+  const handleSendMsg = () => {
+    if(!activeUser){
+      console.error("please select user ")
+    }
+    console.log("Send");
+    let messageObj = {
+      userId: activeUser?.id,
+      text: message
+    };
+    setMessageArr([...messageArr, messageObj]);
+    setMessage("");
+  };
+
   return (
     <div className="App">
-      <div className="Sidebar">
-          <Sidebar/>
-      </div>
-      <div className="Chatbox">
-          <input value={text} onChange={handleInputChange}/>
-          <button onClick={handleSendMsg}>Send Text</button>
+      <div className="SideNav" >
+        <div className="Sidebar" style={{ width: "25%" }}>
+          <div className="Sidebar">
+            <ul className="SidebarList">
+              {SidebarData.map((key, val) => {
+                return (
+                  <li
+                    key={key.id}
+                    className="row"
+                    onClick={() => {
+                      setActiveUser(key);
+                    }}
+                  >
+                    <div id="icon">{key.icon}</div>
+                    <div id="title">{key.name}</div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+        <div  className="message">
+          <h1> {activeUser?.name}</h1>
+          <ul>
+            {messageArr?.length > 0 &&
+              messageArr?.map((message) => {
+                return <li className="list">{message?.text}</li>;
+              })}
+          </ul>
+          <input value={message} onChange={handleInputChange} style={{  marginRight:"20px"}}/>   
+          <button onClick={handleSendMsg}>Send Message</button>
+          <input type="file" onChange={handleUpload} style={{  margin:"20px"}}/>
+                {file && <ImageThumb image={file} />}
+        </div>
       </div>
     </div>
   );
 }
+
+const ImageThumb = ({ image }) => {
+  return <img src={URL.createObjectURL(image)} alt={image.name} />;
+};
 
 export default App;
